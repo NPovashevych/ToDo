53b3108b47f789cc1a1624b4714b896ac3a0de3e
Nataliia Povashevych <interchanal@ukr.net>
2024-03-03 18:32:19 +0200

    todo project done

---
+++ tasks/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.decorators.http import require_POST
+
+from tasks.models import Task, Tag
+
+
+class IndexView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "tasks/task_list.html"
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/task_create.html"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/task_create.html"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/task_delete.html"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "tasks/tag_list.html"
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "tasks/tag_create.html"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "tasks/tag_create.html"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "tasks/tag_delete.html"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+@require_POST
+def toggle_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("tasks:index")
